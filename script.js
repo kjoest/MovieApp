b21fbe3a1e2a1684ad6b9e35a123db83d38a78f2
kyle Joest <kjoest92@gmail.com>
2023-02-15 21:24:53 -0600

    can now display the correct movie information

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-//http://www.omdbapi.com/?apikey=92a2e38f&s=${searchQuery} -- ApiKey/Link for Omdb
+// URL Links
 const apiKey = "fb495745fa6d1d5a8b12ee2b026cbe06";
-const defaultImage = "https://via.placeholder.com/300x450"
+const imageUrl = 'https://image.tmdb.org/t/p/w500';
+const searchUrl = 'https://api.themoviedb.org/3/search/movie?api_key=fb495745fa6d1d5a8b12ee2b026cbe06';
+const defaultImage = "https://i.stack.imgur.com/y9DpT.jpg";
+
+// DOM Elements
 const searchForm = document.querySelector('.search-form');
 const input = document.querySelector('.search-bar');
 
@@ -11,8 +15,8 @@ window.addEventListener('load', (event) => {
   fetchMovies();
 });
 
+// Current Playing Movies
 function fetchMovies() {
-  //const movieList = document.querySelector('.movie-container');
   fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=${currentPage}`)
     .then(response => response.json())
     .then(data => {
@@ -28,7 +32,7 @@ function fetchMovies() {
 
             const movieContainer = document.querySelector('.movie-container');
             const movieID = movieData.id;
-            const posterUrl = movieData.poster_path !== `${defaultImage}` ? `https://image.tmdb.org/t/p/w500${movieData.poster_path}` : "";
+            const posterUrl = movieData.poster_path ? imageUrl + `${movieData.poster_path}` : `${defaultImage}`;
             const title = movieData.title;
             const genres = movieData.genres;
             let genreNames = [];
@@ -48,6 +52,7 @@ function fetchMovies() {
             moviePoster.classList.add('movie-poster');
             moviePoster.setAttribute('src', posterUrl);
             moviePoster.setAttribute('alt', title);
+            moviePoster.setAttribute('data-movie-id', movieID);
 
             const movieTitle = document.createElement('h2');
             movieTitle.classList.add('movie-title');
@@ -99,6 +104,7 @@ function fetchMovies() {
     });
 };
 
+//Search For Movies
 input.addEventListener('input', event => {
   event.preventDefault();
   const searchQuery = input.value;
@@ -115,9 +121,8 @@ input.addEventListener('input', event => {
             .then(response => response.json())
             .then(movieData => {
               const movieContainer = document.querySelector('.movie-container');
-              //document.querySelector('.movie-container').style.display = "none";
               const movieID = movieData.id;
-              const posterUrl = movieData.posterUrl !== "N/A" ? `https://image.tmdb.org/t/p/w500${movieData.poster_path}` : "";
+              const posterUrl = movieData.poster_path ? imageUrl + `${movieData.poster_path}` : `${defaultImage}`;
               const title = movieData.title;
               const genres = movieData.genres;
               let genreNames = [];
@@ -136,6 +141,7 @@ input.addEventListener('input', event => {
               moviePoster.classList.add('movie-poster');
               moviePoster.setAttribute('src', posterUrl);
               moviePoster.setAttribute('alt', title);
+              moviePoster.setAttribute('data-movie-id', movieID);
 
               const movieTitle = document.createElement('h2');
               movieTitle.classList.add('movie-title');
@@ -167,131 +173,129 @@ input.addEventListener('input', event => {
     });
 });
 
+//Display Movie Information when clicking Movie
 let selectedMovie;
 
 const movieContainers = document.querySelectorAll('.movie-container');
+
 movieContainers.forEach(movie => {
-  movie.addEventListener('click', function (event) {
+  movie.addEventListener('click', event => {
     event.preventDefault();
+
     document.querySelector('.search-bar').value = '';
-    const movieTitle = event.currentTarget.closest('.movie-container').querySelector('.movie-title').innerText;
+    document.querySelector('.movie-container-info').style.display = "block";
 
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${movieTitle}&language=en-US`)
+    const target = event.target;
+    const movieID = target.dataset.movieId;
+    if (!movieID) {
+      return;
+    }
+
+    document.querySelector('.movie-container').innerHTML = "";
+
+    fetch(`https://api.themoviedb.org/3/movie/${movieID}?api_key=${apiKey}&language=en-US`)
       .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        document.querySelector('.movie-container').innerHTML = "";
-        if (data.results.length > 0) {
-          const selectedMovies = data.results.filter(movie => movie.title === movieTitle);
-          if (selectedMovies.length === 0) return;
-          selectedMovie = selectedMovies[0];
-          const movieID = selectedMovie.id;
-          console.log(movieID);
+      .then(movie => {
+        console.log(movie);
+        selectedMovie = movie;
+        const id = selectedMovie.id;
+        const posterUrl = selectedMovie.poster_path !== "N/A" ? `https://image.tmdb.org/t/p/w500${selectedMovie.poster_path}` : "";
+        const title = selectedMovie.title;
+        const rating = selectedMovie.vote_average;
+        const description = selectedMovie.overview;
+        const genres = selectedMovie.genres;
+        const releaseDate = selectedMovie.release_date;
+        const runtime = selectedMovie.runtime;
+        let genreNames = [];
+        genres.forEach(genres => {
+          genreNames.push(genres.name);
+        });
 
-          fetch(`https://api.themoviedb.org/3/movie/${movieID}?api_key=${apiKey}&language=en-US`)
-            .then(response => response.json())
-            .then(movie => {
-              selectedMovie = movie;
-              const id = selectedMovie.id;
-              const posterUrl = selectedMovie.poster_path !== "N/A" ? `https://image.tmdb.org/t/p/w500${selectedMovie.poster_path}` : "";
-              const title = selectedMovie.title;
-              const rating = selectedMovie.vote_average;
-              const description = selectedMovie.overview;
-              const genres = selectedMovie.genres;
-              const releaseDate = selectedMovie.release_date;
-              const runtime = selectedMovie.runtime;
-              let genreNames = [];
-              genres.forEach(genres => {
-                genreNames.push(genres.name);
-              });
+        const movieIDInfo = document.createElement('div');
+        movieIDInfo.classList.add('movie-id-info');
+        movieIDInfo.innerText = id;
 
-              const movieIDInfo = document.createElement('div');
-              movieIDInfo.classList.add('movie-id-info');
-              movieIDInfo.innerText = id;
+        const moviePoster = document.createElement('img');
+        moviePoster.classList.add('movie-poster-info');
+        moviePoster.src = posterUrl;
 
-              const moviePoster = document.createElement('img');
-              moviePoster.classList.add('movie-poster-info');
-              moviePoster.src = posterUrl;
-
-              const movieTitle = document.createElement('h2');
-              movieTitle.classList.add('movie-title-info');
-              movieTitle.innerText = title;
+        const movieTitle = document.createElement('h2');
+        movieTitle.classList.add('movie-title-info');
+        movieTitle.innerText = title;
 
 
-              const movieRating = document.createElement('p');
-              movieRating.classList.add('movie-rating-info');
-              movieRating.innerText = `Rating: ${rating}`;
+        const movieRating = document.createElement('p');
+        movieRating.classList.add('movie-rating-info');
+        movieRating.innerText = `Rating: ${rating}`;
 
-              const movieDescription = document.createElement('p');
-              movieDescription.classList.add('movie-description-info');
-              movieDescription.innerText = description;
+        const movieDescription = document.createElement('p');
+        movieDescription.classList.add('movie-description-info');
+        movieDescription.innerText = description;
 
 
-              const movieGenre = document.createElement('p');
-              movieGenre.classList.add('movie-genre-info');
-              movieGenre.innerText = `Genre: ${genreNames.join(', ')}`;
+        const movieGenre = document.createElement('p');
+        movieGenre.classList.add('movie-genre-info');
+        movieGenre.innerText = `Genre: ${genreNames.join(', ')}`;
 
-              const movieReleaseDate = document.createElement('p');
-              movieReleaseDate.classList.add('movie-release-date-info');
-              movieReleaseDate.innerText = `Released: ${releaseDate}`;
+        const movieReleaseDate = document.createElement('p');
+        movieReleaseDate.classList.add('movie-release-date-info');
+        movieReleaseDate.innerText = `Released: ${releaseDate}`;
 
-              const movieRuntime = document.createElement('p');
-              movieRuntime.classList.add('movie-runtime-info');
-              movieRuntime.innerText = `Runtime: ${runtime}`;
+        const movieRuntime = document.createElement('p');
+        movieRuntime.classList.add('movie-runtime-info');
+        movieRuntime.innerText = `Runtime: ${runtime}`;
 
-              fetch(`https://api.themoviedb.org/3/movie/${movieID}/credits?api_key=${apiKey}`)
-              .then(response => response.json())
-              .then(creditsData => {
-                let director = "N/A";
-                let cast = "N/A";
+        fetch(`https://api.themoviedb.org/3/movie/${movieID}/credits?api_key=${apiKey}`)
+          .then(response => response.json())
+          .then(creditsData => {
+            let director = "N/A";
+            let cast = "N/A";
 
-                if(creditsData.crew) {
-                  creditsData.crew.forEach(member => {
-                    if(member.job === "Director") {
-                      director = member.name;
-                      return;
-                    };
-                  });
+            if (creditsData.crew) {
+              creditsData.crew.forEach(member => {
+                if (member.job === "Director") {
+                  director = member.name;
+                  return;
                 };
-
-                if(creditsData.cast) {
-                  cast = creditsData.cast.map(actor => actor.name).join(', ');
-                };
-
-                const movieDirector = document.createElement('p');
-                movieDirector.classList.add('movie-director-info');
-                movieDirector.innerText = `Director: ${director}`;
-
-                const movieCast = document.createElement('p');
-                movieCast.classList.add('movie-cast-info');
-                movieCast.innerText = `Starring: ${cast}`;
-
-                movieContainer.appendChild(movieDirector);
-                movieContainer.appendChild(movieCast);
               });
+            };
 
-              const movieContainer = document.querySelector('.movie-container-info');
-              movieContainer.innerHTML = "";
-              movieContainer.appendChild(movieIDInfo);
-              movieContainer.appendChild(moviePoster);
-              movieContainer.appendChild(movieTitle);
-              movieContainer.appendChild(movieRating);
-              movieContainer.appendChild(movieDescription);
-              movieContainer.appendChild(movieGenre);
-              movieContainer.appendChild(movieReleaseDate);
-              movieContainer.appendChild(movieRuntime);
+            if (creditsData.cast) {
+              cast = creditsData.cast.map(actor => actor.name).join(', ');
+            };
 
-              const prevButton = document.getElementById("previous-button");
-              const nextButton = document.getElementById("next-button");
-              const lastPageButton = document.getElementById("last-page-button")
-              const firstPageButton = document.getElementById("first-page-button")
+            const movieDirector = document.createElement('p');
+            movieDirector.classList.add('movie-director-info');
+            movieDirector.innerText = `Director: ${director}`;
 
-              prevButton.style.display = "none";
-              nextButton.style.display = "none";
-              lastPageButton.style.display = "none";
-              firstPageButton.style.display = "none";
-            });
-        };
+            const movieCast = document.createElement('p');
+            movieCast.classList.add('movie-cast-info');
+            movieCast.innerText = `Starring: ${cast}`;
+
+            movieContainer.appendChild(movieDirector);
+            movieContainer.appendChild(movieCast);
+          });
+
+        const movieContainer = document.querySelector('.movie-container-info');
+        movieContainer.innerHTML = "";
+        movieContainer.appendChild(movieIDInfo);
+        movieContainer.appendChild(moviePoster);
+        movieContainer.appendChild(movieTitle);
+        movieContainer.appendChild(movieRating);
+        movieContainer.appendChild(movieDescription);
+        movieContainer.appendChild(movieGenre);
+        movieContainer.appendChild(movieReleaseDate);
+        movieContainer.appendChild(movieRuntime);
+
+        const prevButton = document.getElementById("previous-button");
+        const nextButton = document.getElementById("next-button");
+        const lastPageButton = document.getElementById("last-page-button")
+        const firstPageButton = document.getElementById("first-page-button")
+
+        prevButton.style.display = "none";
+        nextButton.style.display = "none";
+        lastPageButton.style.display = "none";
+        firstPageButton.style.display = "none";
       });
   });
 });
